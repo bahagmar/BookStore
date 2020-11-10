1c2d2ff823d4883c8ec438938295cc2c1d028ebe
bahagmar <bahaeddinegmar@gmail.com>
2020-11-10 10:15:22 +0100

    method added

---
+++ src/bookstore/Main.java
@@ -0,0 +1,26 @@
+package bookstore;
+
+import java.util.Scanner;
+
+public class Main {
+	//la methode main
+
+	public static void main(String[] args) {
+		//entrer les variables a partitr de l'utilisateur 
+		Scanner c=new Scanner(System.in);
+		//entrer le premier variable qte
+		System.out.println("entrez la quantité");
+		//lire le variable
+		int qte=c.nextInt();
+		//entrer le premier variable qte
+		System.out.println("entrez le prix");
+		//lire le variable
+		float untPrice=c.nextFloat();
+		//instanciation de la classe Produit
+		Produit p=new Produit();
+		//l'affichage du resultat
+		System.out.println("le totale est "+p.calculateTotalePrice(qte, untPrice));
+
+	}
+
+}
---
+++ src/bookstore/Produit.java
@@ -0,0 +1,10 @@
+package bookstore;
+
+public class Produit {
+	//la methode calculate totale Price 
+	float calculateTotalePrice (int qte,float untPrice) {
+		//resultat de la methode 
+		return (qte*untPrice);
+	}
+
+}
